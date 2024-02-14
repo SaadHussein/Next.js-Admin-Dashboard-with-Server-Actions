3ec07f7a9b84284d009c75506016d78fabe923f9
SaadHussein <sa3dhusseein@gmail.com>
2024-02-14 09:44:06 +0200

    Finish design and write database schema for products and user and database connection function

---
+++ app/lib/utils.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connection = {};
+
+export const connetToDB = async () => {
+    try {
+        if (connection.isConnected) {
+            return;
+        }
+
+        const db = await mongoose.connect(process.env.MONGO);
+        connection.isConnected = db.connection[0].readyState;
+    } catch (err) {
+        throw new Error(err);
+    }
+};
